e6729fd18f18a47d51e860f191f64b6fca4e1c7a
gaurav5553 <gaurav5553@gmail.com>
2023-05-12 20:13:05 -0500

    first commit

---
+++ src/main/java/com/dockerized/springboot/pack/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.dockerized.springboot.pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	@GetMapping("/dockerized")
+	public String dockerizedMessage()
+	{
+		return "This is a dockerized spring boot application";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dockerized/springboot/pack/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerized.springboot.pack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
